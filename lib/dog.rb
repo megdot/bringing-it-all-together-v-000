5a962ff8a7ae4063e570c68893e2474c68bdd33b
Megan Dotto <megan.dotto@gmail.com>
2018-06-07 20:53:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,13 +51,26 @@ class Dog
     dog
   end
 
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+
+  end
+
   def self.new_from_db(row)
     id = row[0]
     name = row[1]
     breed = row[2]
     self.new(id: id, name: name, breed: breed)
   end
-
 
 
 
