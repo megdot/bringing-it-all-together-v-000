36b770dda15e647771507d563c390d896d96e48d
Megan Dotto <megan.dotto@gmail.com>
2018-06-07 20:18:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,25 +9,7 @@ class Dog
     @breed = breed
   end
 
-  def self.create_table
-    sql = <<-SQL
-      CREATE TABLE (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        breed TEXT,
-      )
-    SQL
 
-    DB[:conn].execute(sql)
-  end
-
-  def self.drop_table
-    sql = <<-SQL
-    DROP TABLE dogs
-    SQL
-
-    DB[:conn].execute(sql)
-  end
 
 
 end
